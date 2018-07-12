66ea5342bd33feca74da100894b4a468e6cd90ba
celloboiErnest <ernestl8502@gmal.com>
2018-07-12 12:05:57 -0700

    fixed

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ client.on('reconnecting', () => {
 
 client.on('guildMemberAdd', member => {
   let guild = member.guild;
-  guild.defaultChannel.sendMessage(chalk.bgBlue('${member.user.username}, welcome to Wood Division, where it\'s ok to suck because we all suck!'));
+  guild.defaultChannel.sendMessage('${member.user.username}, welcome to Wood Division, where it\'s ok to suck because we all suck!');
 })
 
 client.on('guildBanAdd', (guild, user) => {
@@ -34,7 +34,7 @@ client.on('message', message => {
       message.reply('pong!');
   }
   else if (message.content === 'Introduce yourself Ghoulbot') {
-    message.channel.send(chalk.bgRed('Greetings. I am Ghoulbot1, an Artificial Intelligence created by Harvard University.'));
+    message.channel.send('Greetings. I am Ghoulbot1, an Artificial Intelligence created by Harvard University.');
     message.channel.send({files: ["https://www.diarystore.com/sites/default/files/featureimage-education/harvard_university_1-min.jpg"]})
     message.channel.send(' What are your commands.')
   }
